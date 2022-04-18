a2ad23efa5766c871b07d967416cd9168e81ee0f
Cheng Jen Li <neo@cpshs.org.tw>
2022-04-18 10:08:53 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,6 +3,7 @@ input.onButtonPressed(Button.A, function () {
     radio.sendValue(convertToText(control.deviceSerialNumber()), 0)
 })
 input.onButtonPressed(Button.B, function () {
+    let Student: number[] = []
     for (let index = 0; index <= Student.length; index++) {
         basic.showString("" + (Student[index]))
     }
@@ -12,12 +13,5 @@ radio.onReceivedValue(function (name, value) {
         basic.showIcon(IconNames.Heart)
         basic.showNumber(value)
     }
-    if (Student.indexOf(name) == 0) {
-        Student.push(name)
-        radio.sendValue(name, Student.indexOf(name))
-    }
 })
-let Student: string[] = []
 radio.setGroup(1)
-Student = []
-Student[0] = "T-SE"
